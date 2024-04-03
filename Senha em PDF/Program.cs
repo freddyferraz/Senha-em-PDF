f69fec8fe5821073b64325f0cce8389ac0e8dcf5
freddyferraz <frodrigues@ccprmg.com.br>
2024-04-03 12:28:41 -0300

    Adicionar arquivos de projeto.

---
+++ Senha em PDF/Program.cs
@@ -0,0 +1,14 @@
+﻿/*
+ * Adicione proteção por senha ao documento PDF usando C#
+ */
+using GroupDocs.Merger.Domain.Options;
+using GroupDocs.Merger;
+
+string filePath = @"C:\Users\frodrigues\Downloads\Teste\89246.pdf";
+
+using (Merger merger = new Merger(filePath))
+{
+    AddPasswordOptions addOptions = new AddPasswordOptions("09202224633");
+    merger.AddPassword(addOptions);
+    merger.Save(@"C:\Users\frodrigues\Downloads\Teste\Frederico Hadan.pdf");
+}
